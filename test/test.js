53a7423da3f967ce3159e2df1e5c2f4cb57f935a
Bryan Parker <bryan.parker24@gmail.com>
2013-01-20 11:35:03 -0600

    initial commit

---
+++ test/test.js
@@ -0,0 +1,19 @@
+
+var replace = require('../lib/index2')
+  , assert = require('assert');
+
+var tests = [
+  [ 'Hello ${test} foo', 'world', 'Hello world foo' ],
+  [ 'Hello ${test} foo', { test : 'world' }, 'Hello world foo' ],
+  [ 'Hello ${test} foo ${bar}', { test : 'world', bar : 'bar' }, 'Hello world foo bar' ],
+  [ 'Hello ${data.foo} foo', { data : { foo : 'world', biz : 'baz' } }, 'Hello world foo' ],
+  [ 'Hello ${data.foo.bar} foo', { data : { foo : { bar : 'world' } } },    'Hello world foo' ],
+  [ 'The ${data.one} brown ${data.two} jumped', { data : { one : 'quick', two : 'fox' } }, 'The quick brown fox jumped' ],
+  [ 'The ${data.foo} brown ${data.bar.biz} jumped', { data : { foo : 'quick', bar : { biz : 'fox' }}}, 'The quick brown fox jumped' ]
+];
+
+var i = 0, test = null;
+while(test = tests[i++]) {
+  assert.equal(replace(test[0], test[1]), test[2]);
+}
+
